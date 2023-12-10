9f206ea57554f828507e7c7015b43b71847a7007
DillTheBeast <dillonmaltese@gmail.com>
2023-12-10 14:34:44 -0500

    c

--- test.py
+++ test.py
@@ -14,7 +14,7 @@ class ToDoListApp:
         self.task_entry.grid(row=0, column=0, padx=10, pady=10)
 
         # Add Task Button
-        add_button = tk.Button(root, text="Add Task", command=self.add_task)
+        add_button = tk.Button(root, text="Add Task", command=self.add_main)
         add_button.grid(row=0, column=1, padx=10, pady=10)
 
         # Task List
@@ -22,14 +22,14 @@ class ToDoListApp:
         self.task_listbox.grid(row=1, column=0, columnspan=2, padx=10, pady=10)
 
         # Remove Task Button
-        remove_button = tk.Button(root, text="Remove Task", command=self.remove_task)
+        remove_button = tk.Button(root, text="Remove Task", command=self.remove_main)
         remove_button.grid(row=2, column=0, columnspan=2, pady=10)
 
         # Load tasks from file
-        self.load_tasks()
+        self.load_main()
 
         # Bind double-click to remove task
-        self.task_listbox.bind("<Double-Button-1>", lambda event: self.show_task_window(self.task_listbox.get(tk.ACTIVE)))
+        self.task_listbox.bind("<Double-Button-1>", lambda event: self.show_side_window(self.task_listbox.get(tk.ACTIVE)))
 
         # Add buttons for each task
         for task in self.tasks:
@@ -37,19 +37,19 @@ class ToDoListApp:
             button.grid(row=self.tasks.index(task) + 3, column=0, columnspan=2, pady=5)
 
 
-    def add_task(self):
+    def add_main(self):
         task = self.task_entry.get()
         if task:
             self.tasks.append(task)
             self.task_listbox.insert(tk.END, task)
-            self.save_tasks()
+            self.save_main()
             self.task_entry.delete(0, tk.END)
 
             # Add button for the new task
-            button = tk.Button(self.root, text=task, command=lambda t=task: self.show_task_window(t))
+            button = tk.Button(self.root, text=task, command=lambda t=task: self.show_side_window(t))
             button.grid(row=len(self.tasks) + 2, column=0, columnspan=2, pady=5)
 
-    def show_task_window(self, task):
+    def show_side_window(self, task):
         # Create a new window for the task
         task_window = tk.Toplevel(self.root)
         task_window.title(task)
@@ -59,7 +59,7 @@ class ToDoListApp:
         label.pack(padx=20, pady=20)
 
 
-    def remove_task(self):
+    def remove_main(self):
         selected_index = self.task_listbox.curselection()
         if selected_index:
             task = self.tasks[selected_index[0]]
@@ -67,9 +67,9 @@ class ToDoListApp:
             if confirmation:
                 self.tasks.pop(selected_index[0])
                 self.task_listbox.delete(selected_index)
-                self.save_tasks()
+                self.save_main()
 
-    def load_tasks(self):
+    def load_main(self):
         try:
             with open("todolist.txt", "r") as file:
                 self.tasks = [line.strip() for line in file.readlines()]
@@ -78,8 +78,21 @@ class ToDoListApp:
         except FileNotFoundError:
             pass
 
-    def save_tasks(self):
+    def save_main(self):
         with open("todolist.txt", "w") as file:
+            for task in self.tasks:
+                file.write(f"{task}\n")
+             
+    def add_side(self):
+        task = self.task_entry.get()
+        if task:
+            self.tasks.append(task)
+            self.task_listbox.insert(tk.END, task)
+            self.save_main()
+            self.task_entry.delete(0, tk.END)
+        
+    def save_side(self, filename):
+        with open(filename, "w") as file:
             for task in self.tasks:
                 file.write(f"{task}\n")
 
